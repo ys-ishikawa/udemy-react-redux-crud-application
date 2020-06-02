a45d4bf945ac1b56a41b1e779adf4a5799de66ea
Yosuke Ishikawa <ishikawa.yosuke@bric.jp>
2020-06-02 14:46:09 +0900

    why jsx?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello, World!</h1>
+//     </div>
+//   );
+// }
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
