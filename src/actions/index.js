0c5e3a19bd93e5a4bb05099f913f093939cf04af
Yosuke Ishikawa <ishikawa.yosuke@bric.jp>
2020-06-03 15:14:59 +0900

    implements action and action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-const INCREMENT = "INCREMENT"
-const DECREMENT = "DECREMENT"
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
 
 // Action Creator
 export const increment = () => ({
