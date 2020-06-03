9615ffdb7e49b7ee5ea96171ffdd76efa1a77ae6
Yosuke Ishikawa <ishikawa.yosuke@bric.jp>
2020-06-03 15:12:54 +0900

    learn redux-basic

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+const INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+
+// Action Creator
+export const increment = () => ({
+    type: INCREMENT
+})
+
+
+export const decrement = () => ({
+    type: DECREMENT
+})
